96e344cac53cd8bd0e50ca3d0b9e4308e09cd1a6
Sunggon Park <f2hard3@gmail.com>
2023-11-26 19:36:46 +0900

    Add shape interface practice

---
+++ 08-shape-interface/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+func main() {
+	s := square{
+		sideLength: 5,
+	}
+
+	t := triangle{
+		base:   10,
+		height: 3,
+	}
+
+	printArea(s)
+	printArea(t)
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func printArea(s shape) {
+	fmt.Println("area:", s.getArea())
+}
